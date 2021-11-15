fe206e5fbda0cf5be442a61031b3a2acc65cb06f
Rupa Dey <deyrupa525@gmail.com>
2021-11-15 16:10:01 +0600

    subtraction added

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class hello{
+	public static void main(String[] args)
+	{
+		int result = sum(6,8);
+
+		System.out.println(result);
+	}
+	static int sum(int x,int y)
+	{
+		return x+y;
+	}
+}
